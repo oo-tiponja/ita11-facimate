4a89b698d82722d19195867dafb68206d8620dd1
tiponja <tiponja@oocl.com>
2026-02-27 07:59:18 +0800

    chore: initial project scaffolding with Vite + Ant Design + Axios

---
+++ backend/facimate/src/main/java/com/ita11/backend/FacimateApplication.java
@@ -0,0 +1,13 @@
+package com.ita11.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacimateApplication.class, args);
+	}
+
+}
---
+++ backend/facimate/src/test/java/com/ita11/facimate/FacimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita11.facimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
